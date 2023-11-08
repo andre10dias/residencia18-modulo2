d0f89bb7b5d4b5eefc0a24925e14c6ba71415dbc
andre10dias <andre10dias@ymail.com>
2023-11-08 12:36:10 -0300

    tipos de dados

--- dotnet/helloworld/Program.cs
+++ dotnet/helloworld/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#region helloworld
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+#endregion
+
+#region tipos de dados
+
+int tipoInteiro;
+long tipoInteiroLongo;
+double tipoDouble;
+bool tipoBooleano;
+string tipoString;
+
+tipoInteiro = int.MaxValue;
+System.Console.WriteLine("Valor máximo para o tipo inteiro: "+tipoInteiro);
+
+tipoInteiroLongo = long.MaxValue;
+System.Console.WriteLine("Valor máximo para o tipo inteiro longo: "+tipoInteiroLongo);
+
+//conversão inplícita
+tipoInteiroLongo = tipoInteiro;
+System.Console.WriteLine("Conversão implícita entre int e long: "+tipoInteiroLongo);
+
+tipoString = "100";
+tipoInteiro = int.Parse(tipoString);
+System.Console.WriteLine("Conversão string em int: "+tipoInteiro);
+
+#endregion
